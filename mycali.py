973d573dc3031721c4ab35f91361785e891b28b7
Neutrino331 <ricky40610@gmail.com>
2023-03-06 16:53:36 +0800

    debug

--- mycali.py
+++ mycali.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
 import transforms3d as tfs
 import cv2
 import numpy as np
-import glob
 import asyncio
 import math
-import serial
 import techmanpy
-import time
+
+
 
 robotip = "192.168.10.13"
 
@@ -18,10 +18,21 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((5*7, 3), np.float32)
 objp[:, :2] = np.mgrid[0:7, 0:5].T.reshape(-1, 2)
 
-hand = []
+hand = [98.48088, -580.5756, 137.185, 166.851, 5.214443, -8.11066,
+        86.70583, -580.8532, 213.8269, 177.3208, -31.93295, -27.82692,
+        218.788, -513.8047, 178.1355, 147.2267, -0.3625326, -44.94373,
+        88.23839, -601.2761, 52.41852, 176.3424, -7.565168, -6.573181,
+        192.8963, -518.6313, 197.3694, 141.8427, 16.6704, -27.7069,
+        -6.172165, -565.1449, 111.1683, 148.3893, -21.0301, 41.41455,
+        206.7135, -596.6494, 156.2145, 163.9829, -6.41746, -14.29395,
+        127.5931, -523.8399, 108.8214, 137.9044, -3.270428, 0.4479634,
+        160.6458, -491.9189, 112.2409, 142.6728, 19.38161, 10.55346,
+        50.34186, -554.9487, 82.25433, 141.5474, -0.8874476, 24.1776]
 
 camera = []
-def get_robot_pose():
+
+
+def get_robot_pose(robotip):
     async def get_pose(robotip):
         async with techmanpy.connect_svr(robot_ip=robotip, client_id="info") as conn:
             value = await conn.get_values("Coord_Robot_Tool")
@@ -29,51 +40,58 @@ def get_robot_pose():
         return value
     return asyncio.run(get_pose())
 
+
 def take_picture():
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
-    
+
     else:
         # 擷取影像
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
         else:
-            cv2.namedWindow("live", cv2.WINDOW_AUTOSIZE); # 命名一個視窗，可不寫
-            cv2.imshow("live",frame)
-            cv2.imwrite("test_img.png",frame)
+            #cv2.namedWindow("live", cv2.WINDOW_AUTOSIZE)  # 命名一個視窗，可不寫
+            cv2.imshow("live", frame)
+            cv2.imwrite("test_img.png", frame)
             cv2.waitKey(1000)
             return frame
     cv2.destroyAllWindows()
 
-def move(x,y,z,rx,ry,rz):
-    async def move_to_point(robotip):
-        async with techmanpy.connect_sct(robot_ip=robotip) as conn:
-            await conn.move_to_point_ptp([x, y, z, rx, ry, rz], 0.5, 200)
-    return asyncio.run(move_to_point())
 
-def get_matrix_eular_radu(x,y,z,rx,ry,rz):
-    rmat = tfs.euler.euler2mat(math.radians(rx),math.radians(ry),math.radians(rz))
-    rmat = tfs.affines.compose(np.squeeze(np.asarray((x,y,z))), rmat, [1, 1, 1])
+def move(x, y, z, rx, ry, rz):
+    async def move_to_point():
+        async with techmanpy.connect_sct(robot_ip="192.168.10.13") as conn:
+            await conn.move_to_point_ptp([x, y, z, rx, ry, rz], 0.5, 200)
+    asyncio.run(move_to_point())
+
+
+def get_matrix_eular_radu(x, y, z, rx, ry, rz):
+    rmat = tfs.euler.euler2mat(math.radians(
+        rx), math.radians(ry), math.radians(rz))
+    rmat = tfs.affines.compose(np.squeeze(
+        np.asarray((x, y, z))), rmat, [1, 1, 1])
     return rmat
 
 
 def skew(v):
-    return np.array([[0,-v[2],v[1]],
-                     [v[2],0,-v[0]],
-                     [-v[1],v[0],0]])
+    return np.array([[0, -v[2], v[1]],
+                     [v[2], 0, -v[0]],
+                     [-v[1], v[0], 0]])
+
 
 def rot2quat_minimal(m):
-    quat =  tfs.quaternions.mat2quat(m[0:3,0:3])
+    quat = tfs.quaternions.mat2quat(m[0:3, 0:3])
     return quat[1:]
 
+
 def quatMinimal2rot(q):
-    p = np.dot(q.T,q)
-    w = np.sqrt(np.subtract(1,p[0][0]))
-    return tfs.quaternions.quat2mat([w,q[0],q[1],q[2]])
+    p = np.dot(q.T, q)
+    w = np.sqrt(np.subtract(1, p[0][0]))
+    return tfs.quaternions.quat2mat([w, q[0], q[1], q[2]])
 
 
 axis = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0],
@@ -91,17 +109,23 @@ def draw(img, corners, imgpts):
     img = cv2.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
     return img
 
-for i in range(0,len(hand),6):
+
+for i in range(0, len(hand), 6):
+    move(hand[i], hand[i+1], hand[i+2], hand[i+3], hand[i+4], hand[i+5])
+    time.sleep(5)
     img = take_picture()
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print("gray_img shape:",gray_img.shape)
+    print("gray_img shape:", gray_img.shape)
 
-    cp_int = np.zeros((chessboard_width_num * chessboard_height_num, 3), np.float32)
-    cp_int[:, :2] = np.mgrid[0:chessboard_width_num, 0:chessboard_height_num].T.reshape(-1, 2)
+    cp_int = np.zeros(
+        (chessboard_width_num * chessboard_height_num, 3), np.float32)
+    cp_int[:, :2] = np.mgrid[0:chessboard_width_num,
+                             0:chessboard_height_num].T.reshape(-1, 2)
     # cp_world: corner point in world space, save the coordinate of corner points in world space.
     cp_world = cp_int * 0.02
 
-    ret2, corners = cv2.findChessboardCorners(gray_img, (chessboard_width_num, chessboard_height_num), None)
+    ret2, corners = cv2.findChessboardCorners(
+        gray_img, (chessboard_width_num, chessboard_height_num), None)
     obj_points = []  # the points in world space
     img_points = []  # the points in image space (relevant to obj_points)
     obj_points.append(cp_world)
@@ -109,7 +133,8 @@ for i in range(0,len(hand),6):
 
     if ret2 == True:
         # view the corners
-        cv2.drawChessboardCorners(img, (chessboard_width_num, chessboard_height_num), corners, ret2)
+        cv2.drawChessboardCorners(
+            img, (chessboard_width_num, chessboard_height_num), corners, ret2)
         _, mtx, dist, v_rot, v_trans = cv2.calibrateCamera(
             obj_points, img_points, gray_img.shape[::-1], None, None)
         corners2 = cv2.cornerSubPix(
@@ -123,19 +148,29 @@ for i in range(0,len(hand),6):
         print("Can't find chessboard!")
 
     cv2.imshow('img', img)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
     print("内部参數=", mtx)
     print("扭曲系数=", dist)
-    print(f"第{i}組旋轉向量=", v_rot)
-    print(f"第{i}組平移向量=", v_trans)
+    #print(v_rot[0])
+    print(f"第{i/6}組旋轉向量=", v_rot)
+    print(f"第{i/6}組平移向量=", v_trans)
     print("="*25)
-    camera.append([v_trans[0], v_trans[1], v_trans[2],
-                  v_rot[0], v_rot[1], v_rot[2]])
+    camera.append(v_trans[0][0])
+    camera.append(v_trans[0][1])
+    camera.append(v_trans[0][2])
+    camera.append(v_rot[0][0]) 
+    camera.append(v_rot[0][1]) 
+    camera.append(v_rot[0][2])
+    
 
 Hgs, Hcs = [], []
 
-for i in range(0,len(hand),6):
-    Hgs.append(get_matrix_eular_radu(hand[i],hand[i+1],hand[i+2],hand[i+3],hand[i+4],hand[i+5]))    
-    Hcs.append(get_matrix_eular_radu(camera[i],camera[i+1],camera[i+2],camera[i+3],camera[i+4],camera[i+5]))
+for i in range(0, len(hand), 6):
+    Hgs.append(get_matrix_eular_radu(
+        hand[i], hand[i+1], hand[i+2], hand[i+3], hand[i+4], hand[i+5]))
+    Hcs.append(get_matrix_eular_radu(
+        camera[i], camera[i+1], camera[i+2], camera[i+3], camera[i+4], camera[i+5]))
 
 Hgijs = []
 Hcijs = []
